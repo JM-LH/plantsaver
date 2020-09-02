1999319cd49cf84913330694d7833133afbc55ba
Jannik Malte Meissner <jannik@karen-jones.local>
2020-09-02 09:00:31 +0200

    fixed tree ID

--- moisture-service/moisture_server.py
+++ moisture-service/moisture_server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def get_moisture():
     soil_moisture = random.randint(0, 100)
     # return { 'soil_moisture': soil_moisture }
-    return { 'tree_id_1': '1221'}, {'soil_moisture': soil_moisture }
+    return { 'tree_id': '1221', 'soil_moisture': soil_moisture }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
