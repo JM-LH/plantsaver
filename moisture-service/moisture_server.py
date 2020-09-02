0b1f054236a7b8846b0c13da6f47b2f43afb9cb9
Jannik Malte Meissner <jannik@karen-jones.local>
2020-09-02 08:59:08 +0200

    added experimental tree ID

--- moisture-service/moisture_server.py
+++ moisture-service/moisture_server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def get_moisture():
     soil_moisture = random.randint(0, 100)
-    return { 'soil_moisture': soil_moisture }
+    # return { 'soil_moisture': soil_moisture }
+    return { 'tree_id_1': '1221'}, {'soil_moisture': soil_moisture }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
