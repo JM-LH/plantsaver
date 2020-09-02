b02e34eb2caff3d2e7917eaa1d5e1d556b14af66
Jannik Malte Meissner <jannik@karen-jones.local>
2020-09-02 07:19:37 +0200

    changed JSON name in API

--- moisture-service/moisture_server.py
+++ moisture-service/moisture_server.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_moisture():
-    moisture_per_centage = random.randint(0, 100)
-    return { 'moisture_per_centage': moisture_per_centage }
+    soil_moisture = random.randint(0, 100)
+    return { 'soil_moisture': soil_moisture }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
